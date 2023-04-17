f53e973ae585587c50a6fc41cd3c75d9c5e40ca2
Davit Gelovani <davitgelovani@Davits-Air.netis.cc>
2023-04-17 18:52:02 +0400

    #1 - Initializing Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import GlobalStyle from "./theme/globalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello dshop!</h1>
     </div>
   );
