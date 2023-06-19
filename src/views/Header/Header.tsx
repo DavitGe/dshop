d6d93ede3a0a2d897a455b33be92e943ca8eea6f
Davit_Gelovani <65369123+DavitGe@users.noreply.github.com>
2023-06-19 21:15:40 +0400

    #14 - Display count on cart

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Logo from "../../components/Logo";
 import Menu from "./Menu";
@@ -8,6 +8,7 @@ import { Modal } from "antd";
 import MobileMenu from "./MobileMenu";
 import Wrapper from "../../components/Wrapper";
 import { useNavigate } from "react-router-dom";
+import { cart } from "../../utils/GlobalStorage";
 
 const StyledWrapper = styled(Wrapper)`
   height: 80px;
@@ -31,6 +32,7 @@ const StyledWrapper = styled(Wrapper)`
 const UtilitesWrapper = styled.div`
   height: 80px;
 
+  position: relative;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -47,6 +49,7 @@ const StyledCart = styled(BsCart)`
   margin-bottom: 2px;
   cursor: pointer;
   transition: 0.2s ease;
+
   &:hover {
     transition: 0.2s ease;
     color: ${(props) => props.theme.secondary};
@@ -84,9 +87,34 @@ const MenuIconWrapper = styled.div`
   }
 `;
 
+const CartSizeStyled = styled.span`
+  position: absolute;
+  cursor: pointer;
+  right: -12px;
+  bottom: 50%;
+  width: 16px;
+  height: 16px;
+  background-color: ${(props) => props.theme.effect};
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  padding: 2px;
+`;
+
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [cartSize, setCartSize] = useState(0);
+  const cartData = cart((state: any) => state.data);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const temp = cartData.reduce((sum: number, el: any) => {
+      return sum + el.count;
+    }, 0);
+    setCartSize(temp);
+  }, [cartData]);
 
   const modalOpen = () => {
     setOpen(true);
@@ -114,8 +142,13 @@ const Header = () => {
       <Menu />
       <UtilitesWrapper>
         <Input placeholder="Search" />
-        <StyledCart onClick={() => navigate("/cart")} />
         <StyledUser />
+        <StyledCart onClick={() => navigate("/cart")} />
+        {cartSize !== 0 && (
+          <CartSizeStyled onClick={() => navigate("/cart")}>
+            {cartSize}
+          </CartSizeStyled>
+        )}
       </UtilitesWrapper>
     </StyledWrapper>
   );
